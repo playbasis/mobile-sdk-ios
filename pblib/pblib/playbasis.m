db8eb093dd6f0081c2872a142f9baada4f226885
Wasin Thonkaew <haxpor@gmail.com>
2015-02-03 02:32:29 +0700

    Refactor renew:

--- pblib/pblib/playbasis.m
+++ pblib/pblib/playbasis.m
@@ -96,6 +96,9 @@ static NSString * const BASE_ASYNC_URL = @"https://api.pbapp.net/async/call";
 // - auth
 -(PBRequest *)authInternalBase:(NSString *)apiKey withApiSecret:(NSString *)apiSecret blockingCall:(BOOL)blockingCall syncUrl:(BOOL)syncUrl useDelegate:(BOOL)useDelegate withResponse:(id)response;
 
+// - renew
+-(PBRequest *)renewInternalBase:(NSString *)apiKey withApiSecret:(NSString *)apiSecret blockingCall:(BOOL)blockingCall syncUrl:(BOOL)syncUrl useDelegate:(BOOL)useDelegate withResponse:(id)response;
+
 // - login
 -(PBRequest *)loginInternalBase:(NSString *)playerId syncUrl:(BOOL)syncUrl useDelegate:(BOOL)useDelegate withResponse:(id)response;
 
@@ -417,39 +420,37 @@ static NSString *sDeviceTokenRetrievalKey = nil;
 
 -(PBRequest *)renew:(NSString *)apiKey withApiSecret:(NSString *)apiSecret andDelegate:(id<PBResponseHandler>)delegate
 {
-    // note: the final response is via PBAuthDelegate either by delegate or block
-    // in this case, it's by block
-    apiKeyParam = [[NSString alloc] initWithFormat:@"?api_key=%@", apiKey];
-    authDelegate = [[PBAuthDelegate alloc] initWithPlaybasis:self andDelegate:delegate];
-    NSString *data = [NSString stringWithFormat:@"api_key=%@&api_secret=%@", apiKey, apiSecret];
-    return [self call:@"Auth/renew" withData:data syncURLRequest:YES andDelegate:authDelegate];
+    return [self renewInternalBase:apiKey withApiSecret:apiSecret blockingCall:YES syncUrl:YES useDelegate:YES withResponse:delegate];
 }
 -(PBRequest *)renew:(NSString *)apiKey withApiSecret:(NSString *)apiSecret andBlock:(PBResponseBlock)block
 {
-    // note: the final response is via PBAuthDelegate either by delegate or block
-    // in this case, it's by block
-    apiKeyParam = [[NSString alloc] initWithFormat:@"?api_key=%@", apiKey];
-    authDelegate = [[PBAuthDelegate alloc] initWithPlaybasis:self andBlock:block];
-    NSString *data = [NSString stringWithFormat:@"api_key=%@&api_secret=%@", apiKey, apiSecret];
-    return [self call:@"Auth/renew" withData:data syncURLRequest:YES andDelegate:authDelegate];
+    return [self renewInternalBase:apiKey withApiSecret:apiSecret blockingCall:YES syncUrl:YES useDelegate:NO withResponse:block];
 }
 -(PBRequest *)renewAsync:(NSString *)apiKey withApiSecret:(NSString *)apiSecret andDelegate:(id<PBResponseHandler>)delegate
 {
-    // note: the final response is via PBAuthDelegate either by delegate or block
-    // in this case, it's by block
-    apiKeyParam = [[NSString alloc] initWithFormat:@"?api_key=%@", apiKey];
-    authDelegate = [[PBAuthDelegate alloc] initWithPlaybasis:self andDelegate:delegate];
-    NSString *data = [NSString stringWithFormat:@"api_key=%@&api_secret=%@", apiKey, apiSecret];
-    return [self callAsync:@"Auth/renew" withData:data syncURLRequest:YES andDelegate:authDelegate];
+    return [self renewInternalBase:apiKey withApiSecret:apiSecret blockingCall:NO syncUrl:YES useDelegate:YES withResponse:delegate];
 }
 -(PBRequest *)renewAsync:(NSString *)apiKey withApiSecret:(NSString *)apiSecret andBlock:(PBResponseBlock)block
 {
-    // note: the final response is via PBAuthDelegate either by delegate or block
-    // in this case, it's by block
+    return [self renewInternalBase:apiKey withApiSecret:apiSecret blockingCall:NO syncUrl:YES useDelegate:NO withResponse:block];
+}
+-(PBRequest *)renewInternalBase:(NSString *)apiKey withApiSecret:(NSString *)apiSecret blockingCall:(BOOL)blockingCall syncUrl:(BOOL)syncUrl useDelegate:(BOOL)useDelegate withResponse:(id)response
+{
     apiKeyParam = [[NSString alloc] initWithFormat:@"?api_key=%@", apiKey];
-    authDelegate = [[PBAuthDelegate alloc] initWithPlaybasis:self andBlock:block];
+    
+    // check whether it uses delegate to response back
+    if(useDelegate)
+        authDelegate = [[PBAuthDelegate alloc] initWithPlaybasis:self andDelegate:response];
+    else
+        authDelegate = [[PBAuthDelegate alloc] initWithPlaybasis:self andBlock:response];
+    
     NSString *data = [NSString stringWithFormat:@"api_key=%@&api_secret=%@", apiKey, apiSecret];
-    return [self callAsync:@"Auth/renew" withData:data syncURLRequest:YES andDelegate:authDelegate];
+    
+    // auth call has only delegate response, thus we just check for blocking/non-blocking call
+    if(blockingCall)
+        return [self call:@"Auth" withData:data syncURLRequest:syncUrl andDelegate:authDelegate];
+    else
+        return [self callAsync:@"Auth" withData:data syncURLRequest:syncUrl andDelegate:authDelegate];
 }
 
 -(PBRequest *)playerPublic:(NSString *)playerId withDelegate:(id<PBResponseHandler>)delegate
