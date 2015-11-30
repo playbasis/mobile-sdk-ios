34c066e1be6917dd6e4c24ba69768c6aef289de6
Patsita Kongkaew <patsita.k@playbasis.com>
2015-11-30 16:15:04 +0700

    sample profile demo

---
+++ demoApp/demoApp/ProfileViewController.h
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.h
+//  demoApp
+//
+//  Created by Patsita Kongkaew on 11/24/15.
+//  Copyright © 2015 Maethee Chongchitnant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Playbasis.h"
+@interface ProfileViewController : UIViewController
+@property (strong,nonatomic) IBOutlet UITextView *detailView;
+@property (strong,nonatomic) IBOutlet UIButton *profileInfoBt;
+@property (strong,nonatomic) IBOutlet UIButton *levelBt;
+@end
