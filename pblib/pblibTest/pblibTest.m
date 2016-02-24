0a175a00bfc53f2b4a6507632a943d60552f9719
Wasin Thonkaew <haxpor@gmail.com>
2016-02-24 08:06:43 +0700

    Complete testcase for authentication api

--- pblib/pblibTest/pblibTest.m
+++ pblib/pblibTest/pblibTest.m
@@ -9,7 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "Playbasis.h"
 
-@interface pblibTest : XCTestCase
+@interface pblibTest : XCTestCase <PBAuth_ResponseHandler>
 
 @end
 
@@ -25,8 +25,24 @@
     [super tearDown];
 }
 
+#pragma mark Delegate
+- (void)processResponseWithAuth:(PBAuth_Response *)auth withURL:(NSURL *)url error:(NSError *)error
+{
+    XCTAssertEqual(error, nil, @"error must be nil");
+}
+
 #pragma mark Authentication
-- (void)testAuthenticationViaProtectedResources {
+- (void)testAuthenticationViaProtectedResources_delegate {
+    // set key to decrypt protected resource
+    [Playbasis setProtectedResourcesKey:@"playbasis_2016*"];
+    
+    NSBundle *bundle = [NSBundle bundleWithURL: [[NSBundle bundleForClass:[self class]] URLForResource:@"pblibResource" withExtension:@"bundle"]];
+    NSLog(@"path : %@", [bundle resourcePath]);
+    
+    [[Playbasis sharedPB] authWithDelegate:self bundle:bundle];
+}
+
+- (void)testAuthenticationViaProtectedResources_block {
     // set key to decrypt protected resource
     [Playbasis setProtectedResourcesKey:@"playbasis_2016*"];
     
@@ -34,6 +50,28 @@
     NSLog(@"path : %@", [bundle resourcePath]);
     
     [[Playbasis sharedPB] authWithBlock:^(PBAuth_Response *auth, NSURL *url, NSError *error) {
+        XCTAssertEqual(error, nil, @"error must be nil");
+    } bundle:bundle];
+}
+
+- (void)testAuthenticationViaProtectedResources_delegateAsync {
+    // set key to decrypt protected resource
+    [Playbasis setProtectedResourcesKey:@"playbasis_2016*"];
+    
+    NSBundle *bundle = [NSBundle bundleWithURL: [[NSBundle bundleForClass:[self class]] URLForResource:@"pblibResource" withExtension:@"bundle"]];
+    NSLog(@"path : %@", [bundle resourcePath]);
+    
+    [[Playbasis sharedPB] authWithDelegateAsync:self bundle:bundle];
+}
+
+- (void)testAuthenticationViaProtectedResources_blockAsync {
+    // set key to decrypt protected resource
+    [Playbasis setProtectedResourcesKey:@"playbasis_2016*"];
+    
+    NSBundle *bundle = [NSBundle bundleWithURL: [[NSBundle bundleForClass:[self class]] URLForResource:@"pblibResource" withExtension:@"bundle"]];
+    NSLog(@"path : %@", [bundle resourcePath]);
+    
+    [[Playbasis sharedPB] authWithBlockAsync:^(PBAuth_Response *auth, NSURL *url, NSError *error) {
         XCTAssertEqual(error, nil, @"error must be nil");
     } bundle:bundle];
 }
