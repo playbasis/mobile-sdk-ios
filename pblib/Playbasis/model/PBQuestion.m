8111aca68b6341a091192a98eba0c23d9d61a819
Wasin Thonkaew <haxpor@gmail.com>
2016-08-03 22:14:44 +0700

    Fix unchecking of nil or unset timelimit of question api

--- pblib/Playbasis/model/PBQuestion.m
+++ pblib/Playbasis/model/PBQuestion.m
@@ -18,21 +18,29 @@
         
         // get current node as string
         NSString *timelimitString = (NSString*)currentNode;
-        // split string for ":"
-        NSArray<NSString*> *splits = [timelimitString componentsSeparatedByString:@":"];
         
-        // convert each split into number
-        NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
-        f.numberStyle = NSNumberFormatterDecimalStyle;
-        
-        // hour
-        NSNumber *hour = [f numberFromString:splits[0]];
-        // minute
-        NSNumber *minute = [f numberFromString:splits[1]];
-        // seconds
-        NSNumber *second = [f numberFromString:splits[2]];
-        
-        return [NSNumber numberWithInteger:[hour integerValue]*60*60 + [minute integerValue]*60 + [second integerValue]];
+        if (timelimitString != nil && ![timelimitString isEqualToString:@""])
+        {
+            // split string for ":"
+            NSArray<NSString*> *splits = [timelimitString componentsSeparatedByString:@":"];
+            
+            // convert each split into number
+            NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+            f.numberStyle = NSNumberFormatterDecimalStyle;
+            
+            // hour
+            NSNumber *hour = [f numberFromString:splits[0]];
+            // minute
+            NSNumber *minute = [f numberFromString:splits[1]];
+            // seconds
+            NSNumber *second = [f numberFromString:splits[2]];
+            
+            return [NSNumber numberWithInteger:[hour integerValue]*60*60 + [minute integerValue]*60 + [second integerValue]];
+        }
+        else
+        {
+            return nil;
+        }
     }];
 }
 
