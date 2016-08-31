075452fec31f59455267a88e2d32896bd8c62083
Wasin Thonkaew <haxpor@gmail.com>
2016-08-31 12:23:06 +0700

    Add test engine's rule api with custom field 2

--- pblib/PlaybasisTestSuite/TestEngineApi.m
+++ pblib/PlaybasisTestSuite/TestEngineApi.m
@@ -10,7 +10,7 @@
 #import "Macros.h"
 #import "Playbasis.h"
 
-#define TIMEOUT 6.0f
+#define TIMEOUT 60.0f
 INIT_VARS_STATIC
 
 @interface TestEngineApi : XCTestCase
@@ -49,6 +49,19 @@ INIT_VARS_STATIC
     
     BEGIN_AUTHWRAP
     [PBEngineApi rule:[Playbasis sharedPB] playerId:@"jontestuser" action:@"review" customParamKeys:@[@"level", @"world"] customParamValues:@[@"1", @"1"] andCompletion:^(PBRule * _Nullable result, NSError * _Nullable error) {
+        XCTAssert(error == nil, @"error must be nil");
+        EXP_FULFILL
+    }];
+    END_AUTHWRAP
+    
+    EXP_WAIT(TIMEOUT)
+}
+
+- (void)testRuleWithCustomFields2 {
+    EXP_CREATE(@"rule - custom fields")
+    
+    BEGIN_AUTHWRAP
+    [PBEngineApi rule:[Playbasis sharedPB] playerId:@"mario" action:@"comment" andCompletion:^(PBRule * _Nullable result, NSError * _Nullable error) {
         XCTAssert(error == nil, @"error must be nil");
         EXP_FULFILL
     }];
